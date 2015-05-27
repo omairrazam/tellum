e999c263ef192c9cf0bbe479d30480c135dbecca
Ali Hassan <ali.hassan@virtual-force.com>
2015-05-27 20:52:24 +0500

    enable errors for delivery mails

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,7 +21,7 @@ Tellum::Application.configure do
   config.assets.digest = true
 
   config.action_mailer.default_url_options = { :host => '52.25.164.35:6900' }
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
