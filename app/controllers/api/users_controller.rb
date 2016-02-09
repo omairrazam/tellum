2d16f82f076b75da7e868b3b131c351bbe8d72fa
Ali Hassan <ali.hassan@virtual-force.com>
2016-02-09 16:52:16 +0500

    commented some code

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -74,12 +74,12 @@ class Api::UsersController < Api::ApplicationController
             format.html { redirect_to @user, notice: 'user was successfully updated.' }
             format.json { render json: {:response => {:status=>@message.status,:code=>@message.code,:message=>@message.custom_message,  :user => @user.hide_fields.merge!({:authentication_token => @user.authentication_token, followers_count: UserFollow.where(user_id: @user.id, is_approved: true).count, followings_count: UserFollow.where(follow_id: @user.id, is_approved: true).count})} } }
           end
-        elsif @user.update_attributes(facebook_user_id: params[:request][:user][:facebook_user_id], device_token: params[:request][:user][:device_token])
-          get_api_message "200","updated"
-          respond_to do |format|
-            format.html { redirect_to @user, notice: 'user was successfully updated.' }
-            format.json { render json: {:response => {:status=>@message.status,:code=>@message.code,:message=>@message.custom_message,  :user => @user.hide_fields.merge!({:authentication_token => @user.authentication_token, followers_count: UserFollow.where(user_id: @user.id, is_approved: true).count, followings_count: UserFollow.where(follow_id: @user.id, is_approved: true).count})} } }
-          end
+        # elsif @user.update_attributes(facebook_user_id: params[:request][:user][:facebook_user_id], device_token: params[:request][:user][:device_token])
+        #   get_api_message "200","updated"
+        #   respond_to do |format|
+        #     format.html { redirect_to @user, notice: 'user was successfully updated.' }
+        #     format.json { render json: {:response => {:status=>@message.status,:code=>@message.code,:message=>@message.custom_message,  :user => @user.hide_fields.merge!({:authentication_token => @user.authentication_token, followers_count: UserFollow.where(user_id: @user.id, is_approved: true).count, followings_count: UserFollow.where(follow_id: @user.id, is_approved: true).count})} } }
+        #   end
         else
           get_api_message "501","Error"
           respond_to do |format|
