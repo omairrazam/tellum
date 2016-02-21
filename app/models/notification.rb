8a57c6f7f7838c0b3c1de9511a5fd13c2803147f
Ali Hassan <ali.hassan@virtual-force.com>
2016-02-21 22:15:02 +0500

    added in attribute_accesible

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ActiveRecord::Base
-  attr_accessible :status, :object_name, :user_id, :tag_id, :rating_id, :reveal_id, :comment_id, :is_rejected, :is_accepted, :is_seen, :sender_id, :is_view
+  attr_accessible :status, :object_name, :user_id, :tag_id, :rating_id, :reveal_id, :comment_id, :is_rejected, :is_accepted, :is_seen, :sender_id, :is_view, :is_anonymous_user
   belongs_to :comment
   belongs_to :tag
   belongs_to :user
