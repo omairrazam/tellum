ddefb70b6c43c03692e8aa750b11e98265bb2b9a
kamran hameed <kami.eeze@gmail.com>
2016-05-16 23:23:18 +0500

    updating the code of push notification

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -11,30 +11,25 @@ class Api::CommentsController < Api::ApplicationController
       rating_creator_user_id = Rating.find(params[:request][:rating_id])
       badge_count = rating_creator_user_id.try(:user).try(:badge_count) + 1
       rating_creator_user_id.try(:user).update_attributes badge_count: badge_count
-      # unless rating_creator_user_id.try(:user).id == @user
-      #   # APNS.send_notification(rating_creator_user_id.try(:user).try(:device_token), alert: "#{current_user.try(:full_name)} replied to your comment in #{rating_creator_user_id.try(:tag).try(:tag_line)}",badge: badge_count, sound: "default" )
-      #   if params[:is_anonymous_comment] == true
-      #     Notification.create(user_id: rating_creator_user_id.try(:user).id, comment_id: @comment.id, object_name: "Comment", sender_id: @user, rating_id: rating_creator_user_id.id, is_anonymous_user: true)
-      #   else
-      #     Notification.create(user_id: rating_creator_user_id.try(:user).id, comment_id: @comment.id, object_name: "Comment", sender_id: @user, rating_id: rating_creator_user_id.id, is_anonymous_user: false)
-      #   end
-      # end
+      unless rating_creator_user_id.try(:user).id == @user
+        APNS.send_notification(rating_creator_user_id.try(:user).try(:device_token), alert: "#{current_user.try(:full_name)} replied to your comment in #{rating_creator_user_id.try(:tag).try(:tag_line)}", badge: badge_count, sound: "default")
+        if params[:is_anonymous_comment] == true
+          Notification.create(user_id: rating_creator_user_id.try(:user).id, comment_id: @comment.id, object_name: "Comment", sender_id: @user, rating_id: rating_creator_user_id.id, is_anonymous_user: true)
+        else
+          Notification.create(user_id: rating_creator_user_id.try(:user).id, comment_id: @comment.id, object_name: "Comment", sender_id: @user, rating_id: rating_creator_user_id.id, is_anonymous_user: false)
+        end
+      end
       #send the notification here in every case
       # push_notifications = []
-      commenters = rating_creator_user_id.commenters.group(:id)
+      commenters = rating_creator_user_id.commenters.uniq
       commenters.each do |commenter|
-        if commenter.id != current_user.id
-          if commenter.id == rating_creator_user_id.try(:user).id
-            Notification.create(user_id: rating_creator_user_id.try(:user).id, comment_id: @comment.id, object_name: "Comment", sender_id: @user, rating_id: rating_creator_user_id.id, is_anonymous_user: false)
-            APNS.send_notification(commenter.try(:device_token), alert: "#{current_user.try(:full_name)} replied to your comment in #{rating_creator_user_id.try(:tag).try(:tag_line)}.", badge: badge_count, sound: "default")
+        if commenter.id != current_user.id && commenter.id != rating_creator_user_id.try(:user).id
+          if params[:is_anonymous_comment] == true
+            Notification.create(user_id: commenter.id, comment_id: @comment.id, object_name: "Comment", sender_id: @user, rating_id: rating_creator_user_id.id, is_anonymous_user: true)
+            APNS.send_notification(commenter.try(:device_token), alert: "Anonymous also replied to Anonymous's comment in #{rating_creator_user_id.try(:tag).try(:tag_line)}.", badge: badge_count, sound: "default")
           else
-            if params[:is_anonymous_comment] == true
-              Notification.create(user_id: commenter.id, comment_id: @comment.id, object_name: "Comment", sender_id: @user, rating_id: rating_creator_user_id.id, is_anonymous_user: true)
-              APNS.send_notification(commenter.try(:device_token), alert: "Anonymous also replied to Anonymous's comment in #{rating_creator_user_id.try(:tag).try(:tag_line)}.", badge: badge_count, sound: "default")
-            else
-              Notification.create(user_id: commenter.id, comment_id: @comment.id, object_name: "Comment", sender_id: @user, rating_id: rating_creator_user_id.id, is_anonymous_user: false)
-              APNS.send_notification(commenter.try(:device_token), alert: "#{current_user.try(:full_name)} also replied to #{rating_creator_user_id.try(:user).try(:full_name)}'s comment in #{rating_creator_user_id.try(:tag).try(:tag_line)}.", badge: badge_count, sound: "default")
-            end
+            Notification.create(user_id: commenter.id, comment_id: @comment.id, object_name: "Comment", sender_id: @user, rating_id: rating_creator_user_id.id, is_anonymous_user: false)
+            APNS.send_notification(commenter.try(:device_token), alert: "#{current_user.try(:full_name)} also replied to #{rating_creator_user_id.try(:user).try(:full_name)}'s comment in #{rating_creator_user_id.try(:tag).try(:tag_line)}.", badge: badge_count, sound: "default")
           end
         end
       end
