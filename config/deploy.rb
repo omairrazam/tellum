069957b5bd23b0cdba910e0d63f0452286839c15
Omair <omair@gems.techverx.com>
2016-09-02 12:29:41 +0500

    assets configuration adjusted + capistrano seeds task added in automation

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,25 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
+
+namespace :deploy do
+  desc 'Runs rake db:seed for SeedMigrations data'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
+
+  after 'deploy:migrate', 'deploy:seed'
+end
+
+
+
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
